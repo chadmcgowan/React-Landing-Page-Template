3c121670b6729c8b3b72363e99a729e5da6b7491
chadmcgowan <chad.mcgowan@gmail.com>
2022-01-07 17:33:52 -0500

    fixed margins

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -2,7 +2,7 @@ export const Features = (props) => {
   return (
     <div id='features' className='text-center'>
       <div className='container'>
-        <div className='col-md-10 col-md-offset-1 section-title'>
+        <div className='section-title'>
           <h2>Functional Areas</h2>
         </div>
         <div className='row'>
