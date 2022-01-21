ffd2b03c08bda2506056191f42d4cb3b87466890
chadmcgowan <chad.mcgowan@gmail.com>
2022-01-21 14:24:24 -0500

    added alt attribute to img

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -16,7 +16,7 @@ export const Navigation = (props) => {
             <span className='icon-bar'></span>{' '}
           </button>
           <a className='navbar-brand page-scroll' href='#page-top'>
-            <img src="img/logo.png"/>
+            <img src="img/logo.png" alt="Project Advantage"/>
           </a>{' '}
         </div>
 
